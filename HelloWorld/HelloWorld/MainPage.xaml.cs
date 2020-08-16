63306ba7d27e27e0517002f3cce144d860123d20
Esti Tweg <estictweg@hotmail.com>
2020-08-15 21:36:36 -0400

    Completed first two QuickStart tutorials https://docs.microsoft.com/en-us/xamarin/get-started/quickstarts/single-page?pivots=macos

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+
+        void OnSaveButtonClicked(object sender, System.EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, System.EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
